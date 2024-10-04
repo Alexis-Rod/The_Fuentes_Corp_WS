c88eded29e5c19a32e0cbe580bdc51a10427c506
Alexis Rodriguez Estrada <alexis.rodriguez@thefuentescorp.com.mxm>
2024-10-04 11:47:25 -0600

    Se cambio los valores del archivo conexion.php para que se pueda conectar a la BD

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,10 +1,10 @@
 <?php
     class Conexion{
         public static function Conectar(){
-            define('servidor', 'localhost');
+            define('servidor', '34.174.107.198');
             define('nombre_bd', 'fuentes_group');
-            define('usuario', 'root');
-            define('password', '');
+            define('usuario', 'the-fuentes-corp');
+            define('password', 'TheFuentes2024');
             $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
             try{
                 $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd,usuario,password,$opciones);
