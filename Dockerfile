e8dd4ccb6cf13f1f7d01b30f3b1bce695b1abada
Alexis Rodriguez Estrada <alexis.rodriguez@thefuentescorp.com.mxm>
2024-10-28 09:28:23 -0600

    Resuelto conflicto y actualizado desde el remoto

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM php:7.4-apache
+FROM php:8.2-apache
 
 # Instalar extensiones PHP necesarias
-RUN docker-php-ext-install pdo pdo_mysql
+RUN docker-php-ext-install pdo 
 
 # Habilitar mod_rewrite para Apache
 RUN a2enmod rewrite
 
 # Copiar los archivos de la aplicación
-COPY . /var/www/html/
+COPY ./. /var/www/html/
 
 # Establecer permisos adecuados
 RUN chown -R www-data:www-data /var/www/html
@@ -17,3 +17,8 @@ EXPOSE 80
 
 # Comando para iniciar Apache
 CMD ["apache2-foreground"]
+
+ENV DB_HOST=localhost
+ENV DB_USER=root
+ENV DB_PASSWORD=
+ENV DB_NAME=fuentes_group
--- bd/conexion.php
+++ bd/conexion.php
@@ -1,18 +1,23 @@
 <?php
-class Conexion {
-    public static function Conectar() {
-        $host = getenv('DB_HOST') ?: '34.174.107.198'; // IP de tu Cloud SQL
+class Conexion
+{
+    public static function Conectar()
+    {
+        /* $host = getenv('DB_HOST') ?: '34.174.107.198'; // IP de tu Cloud SQL
         $dbname = getenv('DB_NAME') ?: 'fuentes_group';
         $username = getenv('DB_USER') ?: 'the-fuentes-corp';
-        $password = getenv('DB_PASSWORD') ?: 'TheFuentes2024';
+        $password = getenv('DB_PASSWORD') ?: 'TheFuentes2024'; */
+        define('servidor', 'localhost');
+        define('nombre_bd', 'fuentes_group');
+        define('usuario', 'root');
+        define('password', '');
 
         try {
-            $conexion = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $conexion = new PDO("mysql:host=".servidor.";dbname=".nombre_bd, usuario, password);
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conexion;
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             die("El error de conexion es: " . $e->getMessage());
         }
     }
 }
-?>
