77b0b128640a0a7d497933e0568ccf309f0ed197
Alexis Rodriguez Estrada <alexis.rodriguez@thefuentescorp.com.mxm>
2024-10-28 16:04:46 -0600

    Se modifico el archivo de dockerfile  para que se pueda conectar a la base de datos

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,27 +1,19 @@
 <?php
 
-namespace Google\Cloud\Samples\CloudSQL\MySQL;
-
-use PDO;
-use PDOException;
-use RuntimeException;
-use TypeError;
-
 class Conexion
 {
     public static function Conectar(): PDO
     {
         // Obtener las credenciales de las variables de entorno
-        $host = '34.174.107.198'; // IP de tu Cloud SQL
         $dbname = 'The-Fuentes_Corp';
         $username = 'the-fuentes-corp';
         $password = 'TheFuentes2024';
-        $cloud_sql_conenction_name = 'the-fuentes-corporations:us-south1:the-fuentes-corp';
-        $socket_dir = '/cloudsql';
+        $cloud_sql_connection_name = getenv("CLOUD_SQL_CONNECTION_NAME");
+        $socket_dir = getenv('DB_SOCKET_DIR') ?:'/cloudsql';
 
         try {
             // Configuración del DSN para la conexión
-            $dsn = sprintf('mysql:dbname=%s;unix_socket=%s%s', $dbname, $socket_dir,$cloud_sql_conenction_name);
+            $dsn = sprintf('mysql:dbname=%s;unix_socket=%s%s', $dbname, $socket_dir,$cloud_sql_connection_name);
 
             // Crear la conexión a la base de datos
             $conexion = new PDO($dsn, $username, $password);
