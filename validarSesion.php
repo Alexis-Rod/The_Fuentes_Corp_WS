d70e80f8c4c9a1dd7cb6d85f043b77254d964b87
Alexis Rodriguez Estrada <alexis.rodriguez@thefuentescorp.com.mxm>
2024-10-29 08:34:28 -0600

    Se envia atravez de axios el id del usuario para que se registre en los logs y se adita el validarSesion.php para que rediriga al Login

--- validarSesion.php
+++ validarSesion.php
@@ -2,7 +2,7 @@
 session_start();
 $varemail = $_SESSION["Usuario"];
 if ($varemail == null || $varemail == "") {
-    header("location:https://the-fuentes-corp-ws1-460518334160.us-central1.run.app/login.php");
+    header("location:https:login.php");
     die();
 }
 ?>
