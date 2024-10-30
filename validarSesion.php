7aa388c8fc8af20ff655a936d5b3d374f19166bf
Alexis Rodriguez Estrada <alexis.rodriguez@thefuentescorp.com.mxm>
2024-10-30 16:20:49 -0600

    Se hace el cambio en el archivo validarSesion.php para verificar que funcione cuando se intenta abrir sin antes haber iniciado sesion

--- validarSesion.php
+++ validarSesion.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
-$varemail = $_SESSION["Usuario"];
-if ($varemail == null || $varemail == "") {
-    header("location:https:login.php");
-    die();
+
+// Verifica si la clave "Usuario" está definida
+if (!isset($_SESSION["Usuario"]) || $_SESSION["Usuario"] == "") {
+    // Redirige a login.php si la sesión no está activa
+    header("Location: https://the-fuentes-corp-ws1-460518334160.us-central1.run.app/login.php");
+    exit(); // Usa exit() para asegurarte de que el script se detenga después de la redirección
 }
+
+// El resto de tu código aquí
 ?>
