19ba12536ef2b012dc4469a80ee4279b5d8a06b2
Alexis Rodriguez Estrada <alexis.rodriguez@thefuentescorp.com.mxm>
2024-11-27 12:47:26 -0600

    Se aumenta los digitos de la nueva hoja para evitar que se repita

--- js/enlazar_requisiciones.js
+++ js/enlazar_requisiciones.js
@@ -22,6 +22,33 @@ const appRequesition = new Vue({
         },
         infoObraActiva: function (obrasId) {
             axios.post(url, { accion: 3, obra: obrasId }).then(response => {
+                $('#example').DataTable({
+                    "order": [],
+                    "language": {
+                        "sProcessing": "Procesando...",
+                        "sLengthMenu": "Mostrar _MENU_ registros",
+                        "sZeroRecords": "No se encontraron resultados",
+                        "sEmptyTable": "Ningún dato disponible en esta tabla",
+                        "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                        "sInfoPostFix": "",
+                        "sSearch": "Buscar:",
+                        "sUrl": "",
+                        "sInfoThousands": ",",
+                        "sLoadingRecords": "Cargando...",
+                        "oPaginate": {
+                            "sFirst": "Primero",
+                            "sLast": "Último",
+                            "sNext": "Siguiente",
+                            "sPrevious": "Anterior"
+                        },
+                        "oAria": {
+                            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                        }
+                    }
+                });
                 this.obras = response.data;
                 console.log(this.obras);
             });
@@ -90,33 +117,6 @@ const appRequesition = new Vue({
         this.consultarUsuario(localStorage.getItem("NameUser"));
         this.infoObraActiva(localStorage.getItem("obraActiva"));
         this.consultarInfoPresion(localStorage.getItem("IdPresion"));
-        $('#example').DataTable({
-            "order": [],
-            "language": {
-                "sProcessing": "Procesando...",
-                "sLengthMenu": "Mostrar _MENU_ registros",
-                "sZeroRecords": "No se encontraron resultados",
-                "sEmptyTable": "Ningún dato disponible en esta tabla",
-                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix": "",
-                "sSearch": "Buscar:",
-                "sUrl": "",
-                "sInfoThousands": ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst": "Primero",
-                    "sLast": "Último",
-                    "sNext": "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                }
-            }
-        });
     },
     computed: {
 
