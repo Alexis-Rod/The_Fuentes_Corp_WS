e2bd18fdbd945a7f7d8d39dc842ac2ca7d83a619
Alexis Rodriguez <alexisirv71531@gmail.com>
2025-10-20 15:10:21 -0600

    corregir el tema de crear requisiciones se regresa la correcion ṕues al parecer todo esta correcto

--- js/nueva_hoja.js
+++ js/nueva_hoja.js
@@ -412,7 +412,7 @@ const appRequesition = new Vue({
             console.log(this.Items);
             axios.post(url, { accion: 1, time: this.timeNow, id_emisor: this.Emisor_Id, id_prov: this.Prov_Id, Total: this.Total_Pagar, formaPago: this.FormaPago, fechaSolicitud: FechaReq, items: JSON.stringify(this.Items), idReq: idReq, observaciones: this.observaciones, conceptoUnico: this.conceptoUnicoText })
             .then(response => {
-                this.idHoja = response.data[idReq];
+                this.idHoja = response.data;
                 console.log(response.data);
             }); 
         },
